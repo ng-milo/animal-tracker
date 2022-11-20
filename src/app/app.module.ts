dc59cbc319cd99b4afe1ca148246e62f89088841
m1l0ng <55813244+m1l0ng@users.noreply.github.com>
2022-11-20 14:11:34 -0800

    adsf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,10 @@ import { TableComponent } from './components/table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableDialogComponent } from './components/table-dialog/table-dialog.component';
 
+import { MatDialogModule } from '@angular/material/dialog';
+import { TableReportComponent } from './components/table-report/table-report.component';
+import { PigComponent } from './components/pig/pig.component'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,13 +21,18 @@ import { TableDialogComponent } from './components/table-dialog/table-dialog.com
     MapComponent,
     TableComponent,
     TableDialogComponent,
+    TableReportComponent,
+    PigComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TableDialogComponent } from '../table-dialog/table-dialog.component';
+import { TableReportComponent } from '../table-report/table-report.component';
 
 @Component({
   selector: 'app-table',
@@ -8,18 +9,28 @@ import { TableDialogComponent } from '../table-dialog/table-dialog.component';
   styleUrls: ['./table.component.css']
 })
 
-export class TableComponent {
+export class TableComponent implements OnInit {
   title = 'dialog-app';
 
-  constructor(private matDialog:MatDialog){
+  people:any[]
 
-  }
 
-  showDialog(){
-    this.matDialog.open(TableDialogComponent,{
-      width:'500px',
-      data: "This is a dialog"
-    });
-  }
+  // constructor(private matDialog:MatDialog){
+
+  // }
+
+  // showDialog(){
+  //   this.matDialog.open(TableDialogComponent,{
+  //     width:'500px',
+  //     data: "This is a dialog"
+  //   });
+  // }
+
+  // showReport(){
+  //   this.matDialog.open(TableReportComponent,{
+  //     width:'1200px',
+  //     height: '900px',
+  //   });
+  // }
 }
 
