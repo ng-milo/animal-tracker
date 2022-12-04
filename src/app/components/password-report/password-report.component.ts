21a12b24098e5d673eab9c95a7991f618447799c
m1l0ng <55813244+m1l0ng@users.noreply.github.com>
2022-12-03 22:29:07 -0800

    fasdfasd

--- src/app/components/password-report/password-report.component.ts
+++ src/app/components/password-report/password-report.component.ts
@@ -5,7 +5,8 @@ import { TableDialogComponent } from '../table-dialog/table-dialog.component';
 import { TableReportComponent } from '../table-report/table-report.component';
 import { MatDialogRef } from '@angular/material/dialog';
 import {Md5} from 'ts-md5';
-
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { PopupComponent } from '../popup/popup.component';
 @Component({
   selector: 'app-password-report',
   templateUrl: './password-report.component.html',
@@ -13,21 +14,18 @@ import {Md5} from 'ts-md5';
 })
 export class PasswordReportComponent {
 
-  constructor(private matDialog:MatDialog, private dialogRef: MatDialogRef<Dialog>){}
+  constructor(private matDialog:MatDialog, private dialogRef: MatDialogRef<Dialog>, private snackBar: MatSnackBar){}
 
   showPasswordReport(){
     let password = (<HTMLInputElement>document.getElementById("reportPassword")!).value;
-    (<HTMLInputElement>document.getElementById("reportPassword")!).setCustomValidity("");
-    (<HTMLInputElement>document.getElementById("reportPassword")!).reportValidity();
     const md5 = new Md5();
     if(md5.appendStr(password).end() == "84892b91ef3bf9d216bbc6e88d74a77c"){
       this.dialogRef.close(true);
     }
     else{
-      (<HTMLInputElement>document.getElementById("reportPassword")!).setCustomValidity("Incorrect password");
-      (<HTMLInputElement>document.getElementById("reportPassword")!).reportValidity();
-      (<HTMLInputElement>document.getElementById("reportPassword")!).setCustomValidity("");
-      (<HTMLInputElement>document.getElementById("reportPassword")!).reportValidity();
+      this.snackBar.open("⠀⠀⠀⠀✿ Please enter the correct password ✿", "", {
+        duration: 5000,
+      });
     }
   }
 }
--- src/app/components/password/password.component.ts
+++ src/app/components/password/password.component.ts
@@ -5,6 +5,8 @@ import { TableDialogComponent } from '../table-dialog/table-dialog.component';
 import { TableReportComponent } from '../table-report/table-report.component';
 import { MatDialogRef } from '@angular/material/dialog';
 import {Md5} from 'ts-md5';
+import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { PopupComponent } from '../popup/popup.component';
 
 @Component({
   selector: 'app-password',
@@ -13,21 +15,18 @@ import {Md5} from 'ts-md5';
 })
 export class PasswordComponent {
 
-  constructor(private matDialog:MatDialog, private dialogRef: MatDialogRef<Dialog>){}
+  constructor(private matDialog:MatDialog, private dialogRef: MatDialogRef<Dialog>, private snackBar: MatSnackBar){}
 
   showPasswordReport(){
     let password = (<HTMLInputElement>document.getElementById("password")!).value;
-    (<HTMLInputElement>document.getElementById("password")!).setCustomValidity("");
-      (<HTMLInputElement>document.getElementById("password")!).reportValidity();
     const md5 = new Md5();
     if(md5.appendStr(password).end() == "84892b91ef3bf9d216bbc6e88d74a77c"){
       this.dialogRef.close(true);
     }
     else{
-      (<HTMLInputElement>document.getElementById("password")!).setCustomValidity("Incorrect password");
-      (<HTMLInputElement>document.getElementById("password")!).reportValidity();
-      (<HTMLInputElement>document.getElementById("password")!).setCustomValidity("");
-      (<HTMLInputElement>document.getElementById("password")!).reportValidity();
+      this.snackBar.open("⠀⠀⠀⠀✿ Please enter the correct password ✿", "", {
+        duration: 5000,
+      });
     }
   }
 
